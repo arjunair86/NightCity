e56541ebc19a5118be5390f7ea44af26e7cbe676
Arjun <arjunair86@gmail.com>
2017-06-22 23:44:03 +0530

    make it more flexible: user can now give two input arguments, i.e. no_of_buildings and maximum_height

--- city.sh
+++ city.sh
@@ -1,13 +1,22 @@
-function rand {
-	RANGE=6
+no_of_builds=$1
+height_of_builds=$2
+
+function rand_heights {
+	RANGE=$height_of_builds
 	number=$RANDOM
 	let "number %= $RANGE"
 }
 
-function rand2 {
+function rand_ticks {
 	RANGE=3
 	number2=$RANDOM
 	let "number2 %= $RANGE"
+}
+
+function rand_sky {
+	RANGE=6
+	number3=$RANDOM
+	let "number3 %= $RANGE"
 }
 
 ticks=(█ ▓ ▒ ║)
@@ -16,22 +25,22 @@ sky=("*" " " " " " " " " " ")
 declare -A matrix
 declare -A flag
 
-for((i=0;i<50;i++)) 
+for((i=0;i<$no_of_builds;i++)) 
 do
-	rand
+	rand_heights
 	matrix[$i]=$number
 	flag[$i]=0
 done
 
-for((i=0;i<6;i++)) 
+for((i=0;i<$height_of_builds;i++)) 
 do
-	for((j=0;j<50;j++)) 
+	for((j=0;j<$no_of_builds;j++)) 
 	do
 		if [[ ${matrix[$j]} == 0 ]]
 		then
 			if [[ ${flag[$j]} == 1 ]]
 			then
-				rand2
+				rand_ticks
 				echo -n "${ticks[$number2]}"
 			else
 				flag[$j]=1
@@ -39,8 +48,8 @@ do
 			fi
 		else
 			((matrix[$j]-=1))
-			rand
-			echo -ne "${sky[$number]}"
+			rand_sky
+			echo -ne "${sky[$number3]}"
 		fi
 	done
 	echo -e "\r"
